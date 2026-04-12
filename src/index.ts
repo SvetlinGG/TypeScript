b617d19ae99f830bc93b2bb27756ff784f20ca65
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-04-12 23:06:49 +0200

    Optional chaining

--- src/index.ts
+++ src/index.ts
@@ -96,9 +96,28 @@ function greet(name: string | null){
     else
         console.log('Hola!');
         
-
 }
 greet(null)
+
+// Optional chaining
+
+type Customer = {
+    birthday?: Date
+}
+
+function getCustomer(id: number): Customer | null | undefined{
+    return id === 0 ? null : {birthday: new Date()}
+}
+let customer = getCustomer(1)
+// Property access operator:
+ /// if ( customer !== null && customer !== undefined)
+    //console.log(customer.birthday);
+    console.log(customer?.birthday?.getFullYear());
+
+ /// Optional element access operator
+
+ 
+
 
 
 
