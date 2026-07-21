094ca187db43c96d7a3731d6c919b65a1569c44a
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-21 23:53:58 +0200

    implement interface

--- src/mastering-generics/mastering-generics-LAB/generic-classes.ts
+++ src/mastering-generics/mastering-generics-LAB/generic-classes.ts
@@ -4,23 +4,4 @@ class Collection<T> {
         this.data = elements;
     }
 
-    addElement(el: T){
-        this.data.push(el);
-    }
-
-    removeElemet(el: T){
-        let index = this.data.indexOf(el);
-
-        if( index > -1){
-            this.data.splice(index, 1);
-        }
-    }
-
-    reverseElements(){
-        return this.data.reverse();
-    }
-
-    showElements(){
-        return this.data;
-    }
-}
+    addEle
