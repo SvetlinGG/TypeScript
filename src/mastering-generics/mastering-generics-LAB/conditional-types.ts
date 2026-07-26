95e1bd1984e50408ee1486c4927ec03fd9e861bf
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-26 23:05:53 +0200

    mechanic

--- src/mastering-generics/mastering-generics-LAB/conditional-types.ts
+++ src/mastering-generics/mastering-generics-LAB/conditional-types.ts
@@ -3,6 +3,4 @@
 // - hasAge will be 'yes' if T has prop age: number
 // - hasAge will be 'no' if T DOESNT have prop age
 
-type hasAge<T> = T extends { age: number } ? 'yes' : 'no';
-type validAge = hasAge<{ name: string; age: number }>
-type invalidAge = hasAge<'pencho'>
+type hasAge<T> = 
--- src/mastering-generics/mastering-generics-LAB/mapped-types-using-generics.ts
+++ src/mastering-generics/mastering-generics-LAB/mapped-types-using-generics.ts
@@ -4,4 +4,13 @@ type Colors = { red: string, blue: string};
 //type PartialPoint = { [ K in keyof Point]?: Point[K] };
 //type PartialColor = { [ K in keyof Colors]?: Colors[K] };
 
-t
+type Optional<T> = {[K in keyof T]?: T[K]};
+
+type PartialPoint = Optional<Point>
+type PartialColor = Optional<Colors>
+
+
+//type Optional<T> = { [K in keyof T]?: T[K]};
+
+//type PartialPiont = Optional<Point>;
+//type PartialColors = Optional<Colors>
