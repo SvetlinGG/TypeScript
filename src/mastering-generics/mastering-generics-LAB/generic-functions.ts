4ee9ea5c4ca668be6f7149f6f55164e324ea1912
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-26 15:56:38 +0200

    tupple in generic function

--- src/mastering-generics/mastering-generics-LAB/generic-functions.ts
+++ src/mastering-generics/mastering-generics-LAB/generic-functions.ts
@@ -1,8 +1,18 @@
 
-const takeLast = <T>(array: T[]): T | undefined => {
-    return array.pop();
+// const takeLast = <T>(array: T[]): T | undefined => {
+//     return array.pop();
+// }
+
+// const simple = takeLast(['Hello', 'World', 'TypeScript'])
+// const secondSimple = takeLast([1, 2, 3, 4]);
+// console.log(simple, secondSimple);
+
+
+const makeTupple = <T, V>(a: T, b: V): [T, V] => {
+    return [a, b]
 }
 
-const simple = takeLast(['Hello', 'World', 'TypeScript'])
-const secondSimple = takeLast([1, 2, 3, 4]);
-console.log(simple, secondSimple);
+const firstTupple = makeTupple(1, 'a')
+const secondTupple = makeTupple('b', '3');
+console.log(firstTupple, secondTupple);
+
