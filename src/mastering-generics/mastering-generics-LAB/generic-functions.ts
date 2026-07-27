19373c460253933ceb6b6d43f1d46c6bed0f39de
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-27 21:38:39 +0200

    final task

--- src/mastering-generics/mastering-generics-LAB/generic-functions.ts
+++ src/mastering-generics/mastering-generics-LAB/generic-functions.ts
@@ -5,14 +5,4 @@
 
 // const simple = takeLast(['Hello', 'World', 'TypeScript'])
 // const secondSimple = takeLast([1, 2, 3, 4]);
-// console.log(simple, secondSimple);
-
-
-const makeTupple = <T, V>(a: T, b: V): [T, V] => {
-    return [a, b]
-}
-
-const firstTupple = makeTupple(1, 'a')
-const secondTupple = makeTupple('b', '3');
-console.log(firstTupple, secondTupple);
-
+// console.log(simpl
