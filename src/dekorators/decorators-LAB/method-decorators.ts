4fccd0bcf6d146bfc69f78e96f53316a2657afa8
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-28 23:20:01 +0200

    method decorators

---
+++ src/dekorators/decorators-LAB/method-decorators.ts
@@ -0,0 +1,20 @@
+class Num {
+    constructor(private _number: number) { }
+    @add10 
+    getNumber() { 
+        return this._number; 
+    }
+}
+function add10(target: Object, name: string, descriptor: PropertyDescriptor) {
+    let originalMethod = descriptor.value;
+
+    descriptor.value = function (...arg: any[]){
+        let result = originalMethod.call(this, arg);
+        result += 10;
+        return result;
+    }
+
+    return descriptor;
+};
+let num = new Num(20);
+console.log(num.getNumber()); 
