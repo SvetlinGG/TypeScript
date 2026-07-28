d5b7724a740bb61ef59de7b79e80b24a4fe313ae
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-28 15:03:19 +0200

    add inputs

--- src/mastering-generics/mastering-generics-EXE/holiday-manager.ts
+++ src/mastering-generics/mastering-generics-EXE/holiday-manager.ts
@@ -54,3 +54,29 @@ class PlannedHoliday implements Holiday {
         return `Holiday: ${startDate} - ${endDate}`;
     }
 }
+
+
+
+// let holiday = new PlannedHoliday(new Date(2024, 1, 1), new Date(2024, 1, 4));
+// let holiday2 = new PlannedHoliday(new Date(2025, 3, 14), new Date(2025, 3, 17));
+// let holidayManager = new HolidayManager<Holiday, TravelVacation>();
+// holidayManager.reserveVacation(holiday, TravelVacation.Abroad);
+// holidayManager.reserveVacation(holiday2, TravelVacation.InCountry);
+// console.log(holidayManager.listReservations())
+
+// let holiday = new PlannedHoliday(new Date(2022, 10, 11), new Date(2022, 10, 18));
+// let holiday2 = new PlannedHoliday(new Date(2024, 5, 18), new Date(2024, 5, 22));
+// let holidayManager = new HolidayManager<Holiday, BeachVacation>();
+// holidayManager.reserveVacation(holiday, BeachVacation.ScubaDiving);
+// holidayManager.reserveVacation(holiday2, BeachVacation.Sea);
+// console.log(holidayManager.listReservations())
+
+// let holiday3 = new PlannedHoliday(new Date(2021, 3, 14), new Date(2020, 3, 17));
+// let holiday4 = new PlannedHoliday(new Date(2024, 2, 1), new Date(2024, 1, 4));
+
+// let holiday = new PlannedHoliday(new Date(2024, 1, 1), new Date(2024, 1, 4));
+// let holiday2 = new PlannedHoliday(new Date(2025, 3, 14), new Date(2024, 3, 17));
+// let holidayManager = new HolidayManager<Holiday, MountainVacation>();
+// holidayManager.reserveVacation(holiday, BeachVacation.ScubaDiving);
+// holidayManager.reserveVacation(holiday2, TravelVacation.InCountry);
+// console.log(holidayManager.listReservations())
