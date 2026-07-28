fbd48b34b85011a0ec2927faf646204bca069818
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-28 23:01:26 +0200

    extending class definition

---
+++ src/dekorators/decorators-LAB/extending-class-definition.ts
@@ -0,0 +1,13 @@
+export
+@addTitle class Person { constructor(public name: string){} }
+
+function addTitle<T extends new(...args: any[]) => {}>(constructor: T){
+    return class extends constructor {
+        constructor(...args: any[]){
+            super('Sir/Madam ' + args[0], ...args.slice(1))
+        }
+    }
+}
+
+let person = new Person('George');
+console.log(person.name);
