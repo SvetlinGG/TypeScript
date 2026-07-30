bf92d4a1aaf23ff53c34d977e77b23736510064f
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-07-30 22:06:22 +0200

    ts types

--- src/introduction-to-typescript/introduction-to-typescript-LAB/primitives.ts
+++ src/introduction-to-typescript/introduction-to-typescript-LAB/primitives.ts
@@ -1,6 +1,27 @@
+export
+// definition of variables
 let num: number = 5;
 console.log(num);
 
 
 // string
 let greeting: string = 'Hello TypeScript';
+
+// boolean
+let isOpen: boolean = true;
+
+// number 
+ let count: number = 42;
+
+// bigint
+let bigCount: bigint = 1237834647493837n;
+
+// null
+let npthingHere: null = null;
+
+// symbol
+
+let uniqueKey: symbol = Symbol("unique")
+
+
+
