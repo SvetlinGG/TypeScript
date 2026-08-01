4f5ac3caf20a07052f2899e534f726e8be76d81c
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-01 22:19:49 +0200

    final summarize person

--- src/introduction-to-typescript/introduction-to-typescript-EXE/04-summarize-person.ts
+++ src/introduction-to-typescript/introduction-to-typescript-EXE/04-summarize-person.ts
@@ -8,8 +8,21 @@ function summarizePerson(
     workInfo?: string[]
 ): [number, string, number, string, string]{
 
+    const fullName = middleName
+    ? `${firstName} ${middleName} ${lastName}`
+    : `${firstName} ${lastName}`;
+
+    const transformedHobbies = 
+    !hobbies || hobbies.length === 0 ? "-" : hobbies.join(", ");
+
+    const transformedWorkInfo = 
+    !workInfo || workInfo.length === 0 ? "-"
+    : `${workInfo[0]} -> ${workInfo[1]}`
+
+
+    return [id, fullName, age, transformedHobbies, transformedWorkInfo  ]
 }
-console.log(summarizePerson(12, 'Eliot', 'Des', 20, 'Braylen', ['tennis', 'football', 'hiking'], ['Sales Consultant', 2500]))
+//console.log(summarizePerson(12, 'Eliot', 'Des', 20, 'Braylen', ['tennis', 'football', 'hiking'], ['Sales Consultant', 2500]))
 console.log(summarizePerson(20, 'Mary', 'Trent', 25, undefined, ['fitness', 'rowing']))
 console.log(summarizePerson(21, 'Joseph', 'Angler', 28))
-console.log(summarizePerson(21, 'Kristine', 'Neva', 23, ''))
+//console.log(summarizePerson(21, 'Kristine', 'Neva', 23, ''))
