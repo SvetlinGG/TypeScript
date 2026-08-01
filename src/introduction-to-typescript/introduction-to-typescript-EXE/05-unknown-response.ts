7ae53824e8ef8f2f4651f4a039f6c77b9848d7ae
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-01 22:36:52 +0200

    complete unknown response

--- src/introduction-to-typescript/introduction-to-typescript-EXE/05-unknown-response.ts
+++ src/introduction-to-typescript/introduction-to-typescript-EXE/05-unknown-response.ts
@@ -1,5 +1,9 @@
 function unknownResponse(param: unknown): string{
 
+    if ( param && typeof param === 'object' && 'value' in param && typeof param.value === 'string'){
+        return param.value;
+    }
+    return "-";
 }
 
 console.log(unknownResponse({ code: 200, text: 'Ok', value: [1, 2, 3] }))
