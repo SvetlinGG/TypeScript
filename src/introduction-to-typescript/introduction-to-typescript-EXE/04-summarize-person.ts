f78b771d9570a0e66d34b1674320b89a26e07e37
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-01 22:04:20 +0200

    summarize person

---
+++ src/introduction-to-typescript/introduction-to-typescript-EXE/04-summarize-person.ts
@@ -0,0 +1,15 @@
+function summarizePerson(
+    id: number,
+    firstName: string,
+    lastName: string,
+    age: number,
+    middleName?: string[],
+    hobbies?: string[],
+    workInfo?: string[]
+): [number, string, number, string, string]{
+
+}
+console.log(summarizePerson(12, 'Eliot', 'Des', 20, 'Braylen', ['tennis', 'football', 'hiking'], ['Sales Consultant', 2500]))
+console.log(summarizePerson(20, 'Mary', 'Trent', 25, undefined, ['fitness', 'rowing']))
+console.log(summarizePerson(21, 'Joseph', 'Angler', 28))
+console.log(summarizePerson(21, 'Kristine', 'Neva', 23, ''))
