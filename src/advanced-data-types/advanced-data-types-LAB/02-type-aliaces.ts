18cae7573c87219448c865204ddf3d5754f5a80b
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-02 21:43:13 +0200

    mapped types

---
+++ src/advanced-data-types/advanced-data-types-LAB/02-type-aliaces.ts
@@ -0,0 +1,97 @@
+type User = { 
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+const user1: User = {
+    firstName: 'Svetlin',
+    lastName: 'Garabedyan',
+    email: 'svetlingarabedyan@yahoo.com'
+}
+
+const user2: User = {
+    firstName: 'Toni',
+    lastName: 'Ilivanova',
+    email: 'toni_77@abv.bg'
+}
+
+type Address = {
+    country: string;
+    city: string;
+    street: string
+}
+
+const addresss1: Address = {
+    country: 'Bulgaria',
+    city: 'Plovdiv',
+    street: 'Mesemvria'
+}
+
+type Person = User & Address;
+
+const person1: Person = {
+    firstName: 'Gabi',
+    lastName: 'Garabedyan',
+    email: 'gabigarabedi@gmail.com',
+    country: 'Japan',
+    city: 'Tokyo',
+    street: 'niakakva',
+}
+console.log(person1);
+
+
+
+
+// type User = {
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+// }
+
+// const user1: User = {
+//     firstName: 'Svetlin',
+//     lastName: 'Garabedyan',
+//     email: 'svetlinb@gmail.com'
+// }
+
+// const user2: User = {
+//     firstName: 'Toni',
+//     lastName: 'Valentinova',
+//     email: 'toni@abv.bg'
+// }
+
+// type Address = {
+//     country: string;
+//     city?: string;
+// }
+
+// const address1: Address = {
+//     country: 'Bulgaria',
+//     city: 'Sofia'
+// }
+
+// const address2: Address = {
+//     country: 'Bulgaria',
+//     city: 'Burgas'
+// }
+
+// type Person = User & Address;
+
+// const person1: Person = {
+//     firstName: 'Svetlin',
+//     lastName: 'Garabedyan',
+//     email: 'svetlinb@gmail.com',
+//     country: 'Bulgaria',
+//     city: 'Sofia'
+// }
+// console.log(person1);
+
+// type AddressKeys = keyof Address
+
+// function printCity(arg: User | Person){
+//     if ('city' in arg) {
+//         console.log(arg.city);
+//     }
+// }
+// printCity(person1)
---
+++ src/advanced-data-types/advanced-data-types-LAB/03-mapped-types.ts
@@ -0,0 +1,12 @@
+export{}
+type Point = {
+    x: number;
+    y: number;
+    z: number;
+}
+
+const poiunt1: Point = {
+    x: 5,
+    y: 10,
+    z: -5
+}
