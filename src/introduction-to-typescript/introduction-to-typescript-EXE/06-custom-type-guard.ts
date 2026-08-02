229e77737b2ed6505f4097214c9ecc71aa45e5c8
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-02 15:05:22 +0200

    complete custom type guard

--- src/introduction-to-typescript/introduction-to-typescript-EXE/06-custom-type-guard.ts
+++ src/introduction-to-typescript/introduction-to-typescript-EXE/06-custom-type-guard.ts
@@ -2,6 +2,14 @@ function customTypeGuard(param: unknown): param is string{
     return (
         Array.isArray(param) &&
         param.length > 0 &&
-        typeof param[0] === 'string'
+        param.every((el) => typeof el === 'string')
     )
 }
+console.log(customTypeGuard({}));
+console.log(customTypeGuard({ test: "one" }));
+console.log(customTypeGuard([]));
+console.log(customTypeGuard(undefined));
+console.log(customTypeGuard(null));
+console.log(customTypeGuard([12, 13]));
+console.log(customTypeGuard(["test", 123]));
+console.log(customTypeGuard(["a", "b", "c"]));
