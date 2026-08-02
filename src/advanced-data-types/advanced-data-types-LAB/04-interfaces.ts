f91c9bca4660848ee425f2737675a97fecf9c27a
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-02 21:59:04 +0200

    use interfaces

---
+++ src/advanced-data-types/advanced-data-types-LAB/04-interfaces.ts
@@ -0,0 +1,16 @@
+export
+
+interface Person {
+    firstName: string,
+    lastName: string,
+    age: number,
+}
+
+let person1: Person = {
+    firstName: 'Svetlin',
+    lastName: 'Garabedyan',
+    age: 53
+}
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
