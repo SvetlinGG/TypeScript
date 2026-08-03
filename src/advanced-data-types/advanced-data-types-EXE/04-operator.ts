55ed96739a312ecd3cae80be77febc85b9af20dc
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-03 02:08:25 +0200

    fix the issue

--- src/advanced-data-types/advanced-data-types-EXE/04-operator.ts
+++ src/advanced-data-types/advanced-data-types-EXE/04-operator.ts
@@ -1,9 +1,22 @@
+
+
 function operator(
     param: string | number | string[],
     operation: 'Index' | 'Length' | 'Add',
-    operand: number
+    operand: number,
 ){
 
+    switch(operation){
+        case 'Index':
+            if (typeof param !== 'number') return param[operand];
+            break;
+        case 'Length':
+            if (typeof param !== 'number') return param.length % operand;
+            break;
+        case 'Add':
+            if (!Array.isArray(param)) return Number(param) + operand;
+            break;
+    }
 }
 
 console.log(operator(['First', 'Second', 'Third'], 'Index', 1))
