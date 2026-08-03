a83c2f69fc135c47464a36836354e4b82d750a9d
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-03 04:02:33 +0200

    car diagnostics

--- src/advanced-data-types/advanced-data-types-EXE/05-car-diagnostics.ts
+++ src/advanced-data-types/advanced-data-types-EXE/05-car-diagnostics.ts
@@ -14,6 +14,38 @@ interface Engine {
 }
 
 interface AdditionalProps {
-    partName: 'Engine' | 'Car Body' | 'Tyres';
-    runDiagnostics(): string;
+    partName: 'Engine' | 'Car Body' | 'Tires';
+    runDiagnostics: () =>  string;
 }
+
+function runDiagnostics(this: {partName: string}): string{
+    return this.partName;
+}
+
+function carDiagnostics(
+    carBody: CarBody & AdditionalProps,
+    tires: Tires & AdditionalProps,
+    engine: Engine & AdditionalProps,
+){
+    console.log(carBody.runDiagnostics());
+    console.log(tires.runDiagnostics());
+    console.log(engine.runDiagnostics());
+    
+
+}
+
+carDiagnostics(
+  {
+    material: "aluminum",
+    state: "scratched",
+    partName: "Car Body",
+    runDiagnostics,
+  },
+  {
+    airPressure: 30,
+    condition: "needs change",
+    partName: "Tires",
+    runDiagnostics,
+  },
+  { horsepower: 300, oilDensity: 780, partName: "Engine", runDiagnostics },
+);
