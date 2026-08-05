610a113c9e7069c88477427748fb7728926c318e
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-05 15:10:12 +0200

    complete class counter

---
+++ src/oop-in-typescript/oop-in-typescript-exe/07-class-counter.ts
@@ -0,0 +1,35 @@
+class Counter {
+    private static count = 0;
+
+    static increment(){
+        Counter.count ++;
+    }
+
+    static getCount(){
+        return Counter.count;
+    }
+}
+Counter.increment();
+Counter.increment();
+console.log(Counter.getCount());
+
+
+
+
+
+
+
+// class Counter {
+//     private static count: number = 0;
+
+//     static increment(){
+//         Counter.count ++;
+//     }
+
+//     static getCount(){
+//        return Counter.count;
+//     }
+// }
+// Counter.increment();
+// Counter.increment();
+// console.log(Counter.getCount());
