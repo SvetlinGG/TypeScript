7afe4b1e8530d7b0c3739af2de329715c33fe7a8
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-05 22:59:35 +0200

    complete task abstract class

--- src/oop-in-typescript/oop-in-typescript-exe/09-abstract-class.ts
+++ src/oop-in-typescript/oop-in-typescript-exe/09-abstract-class.ts
@@ -1,6 +1,43 @@
 
+abstract class Shape {
+    color: string;
+    abstract getArea(): number;
 
+    constructor(color: string){
+        this.color = color;
+    }
+}
 
+class Circle extends Shape {
+    public radius: number;
+    constructor(color: string, radius: number){
+        super(color);
+        this.radius = radius;
+    }
+
+    getArea(): number{
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
+class Rectangle extends Shape {
+    sideA: number;
+    sideB: number;
+
+    constructor(color: string, sideA: number, sideB: number){
+        super(color)
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+    getArea(): number {
+        return this.sideA * this.sideB
+    }
+}
+
+const circle = new Circle("red", 5);
+console.log(circle.getArea());
+
+const rectangle = new Rectangle("blue", 4, 6);
+console.log(rectangle.getArea());
 
 
 
