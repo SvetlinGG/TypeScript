0ac3b4fd11b79a9c1dd26c79a31c7003c0207892
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-05 14:16:59 +0200

    complete abstraction

---
+++ src/oop-in-typescript/oop-in-typescript-exe/05-abstraction.ts
@@ -0,0 +1,26 @@
+interface Animal {
+    makeSound(): string;
+}
+
+class Dog implements Animal {
+    makeSound(): string {
+        return 'Woof';
+    }
+}
+
+const dog = new Dog();
+console.log(dog.makeSound());
+
+// interface Animal {
+//     makeSound(): string;
+    
+// }
+
+// class Dog implements Animal {
+//     makeSound(): string {
+//         return 'Woof';
+//     }
+// }
+
+// const dog = new Dog();
+// console.log(dog.makeSound());
