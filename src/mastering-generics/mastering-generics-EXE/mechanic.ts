ff5d4ebf9b25c4efbc7443758fc98df6307c799e
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-06 20:34:14 +0200

    clean code

--- src/mastering-generics/mastering-generics-EXE/mechanic.ts
+++ src/mastering-generics/mastering-generics-EXE/mechanic.ts
@@ -1,14 +1,4 @@
-interface BaseTechnicalInspectionData {
-    engine: { horsepower: number};
-    tires: { model: string, airPressure: number};
-    body: { material: string};
-}
 
-class Mechanic<T extends BaseTechnicalInspectionData> {
-    technicalInspection(_car: T): boolean{
-        return true;
-    }
-}
 
 let mechanic = new Mechanic();
 let someCar = { engine: { horsepower: 350, type: 'diesel' }, tires: { model: 'BRIT', airPressure: 33 }, body: { material: 'aluminum' } };
