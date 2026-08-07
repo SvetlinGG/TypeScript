2345bd438d487d0bc38c5e3cab0baecba4a4841e
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-08 00:04:39 +0200

    make method equipWeapon

--- exam-prep/TypeScript-Exam-Prep-Resources/src/game-manager.ts
+++ exam-prep/TypeScript-Exam-Prep-Resources/src/game-manager.ts
@@ -14,4 +14,20 @@ export class GameManager {
         this.stash.add(weapon);
         return `Weapon "${weapon.name}" (ID: ${weapon.id}) added to the guild stash.`
     }
+
+    public equipWeapon(heroId: number, weaponId: number): string{
+        const hero = this.heroes.find(h => h.id === heroId);
+
+        if(!hero){
+            return `ERROR: Hero with ID ${heroId} not found.`;
+        }
+        const weapon = this.stash.take(weaponId);
+
+        if(!weapon){
+            return `ERROR: Weapon with ID ${weaponId} not found in the stash.`;
+        }
+
+        hero.weapons.push(weapon);
+        return `Weapon ${weapon.name}" equipped to hero "${hero.name}".`;
+    }
 }
